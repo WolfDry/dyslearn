abc97cc3d0b0dd8e9b5b754ace4bd9e7a1bfb266
Benjamin <benjamindeltour22@gmail.com>
2024-09-08 13:23:29 +0200

    scanQR page style is done

--- src/pages/ScanQR.tsx
+++ src/pages/ScanQR.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, Button, TouchableOpacity } from 'react-native';
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { View, StyleSheet, Text, Button } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
 import { useAuth } from '../context/AuthContext';
 import JWT from 'expo-jwt'
+import { styles } from '../../assets/style/style';
+import { Svg, Path } from 'react-native-svg';
+import { blue } from '../../assets/style/style';
+
 
 const ScanQR: React.FC = () => {
   const [permission, requestPermission] = useCameraPermissions();
@@ -15,15 +19,11 @@ const ScanQR: React.FC = () => {
 
   if (!permission.granted) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.message}>Nous avons besoins de votre permission pour ustiliser la caméra</Text>
+      <View style={style.container}>
+        <Text style={style.message}>Nous avons besoins de votre permission pour ustiliser la caméra</Text>
         <Button onPress={requestPermission} title="Donné l'accès" />
       </View>
     );
-  }
-
-  function rescan() {
-    setScanned(false)
   }
 
   const handleBarCodeScan = async ({ data }: { type: string, data: string }) => {
@@ -36,7 +36,7 @@ const ScanQR: React.FC = () => {
       try {
         const decoded = JWT.decode(jsonToken, secretKey)
         console.log(decoded)
-        const loginResult = await login(decoded.email, decoded.password)    
+        const loginResult = await login(decoded.email, decoded.password)
         console.log(loginResult)
       } catch (error) {
         console.error('erreur de déchiffrement', error)
@@ -45,27 +45,77 @@ const ScanQR: React.FC = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <CameraView style={styles.camera} facing={'back'} autofocus='on' barcodeScannerSettings={{ barcodeTypes: ['qr'] }} onBarcodeScanned={handleBarCodeScan}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={rescan}>
-            <Text style={styles.text}>Rescanner le QrCode</Text>
-          </TouchableOpacity>
+    <View style={[styles.justifyContentCenter, styles.flex_1, styles.center]}>
+      <View style={[styles.center, style.container]}>
+        <Text style={styles.title}>Scanner le QR code</Text>
+        <View style={[styles.flex_1, styles.full, style.cameraContainer]}>
+          <View style={[style.blueBorderTopLeft]} >
+            <Svg width="50" height="50" viewBox="0 0 50 50" fill="none">
+              <Path d="M47.5 2.5H2.5V47.5" stroke={blue} strokeWidth={5} strokeLinecap={"round"} strokeLinejoin={"round"} />
+            </Svg>
+          </View>
+          <View style={[style.blueBorderTopRight]} >
+            <Svg width="50" height="50" viewBox="0 0 50 50" fill="none">
+              <Path d="M2.5 2.5H47.5V47.5" stroke={blue} strokeWidth={5} strokeLinecap={"round"} strokeLinejoin={"round"} />
+            </Svg>
+          </View>
+          <View style={[style.blueBorderBottomLeft]} >
+            <Svg width="50" height="50" viewBox="0 0 50 50" fill="none">
+              <Path d="M47.5 47.5H2.5V2.5" stroke={blue} strokeWidth={5} strokeLinecap={"round"} strokeLinejoin={"round"} />
+            </Svg>
+          </View>
+          <View style={[style.blueBorderBottomRight]} >
+            <Svg width="50" height="50" viewBox="0 0 50 50" fill="none">
+              <Path d="M2.5 47.5H47.5V2.5" stroke={blue} strokeWidth={5} strokeLinecap={"round"} strokeLinejoin={"round"} />
+            </Svg>
+          </View>
+          <CameraView style={style.camera} facing={'back'} autofocus='on' barcodeScannerSettings={{ barcodeTypes: ['qr'] }} onBarcodeScanned={handleBarCodeScan} />
         </View>
-      </CameraView>
+      </View>
     </View>
   )
 }
 
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    width: 287,
+    height: 455,
+    gap: 20,
   },
   message: {
     textAlign: 'center',
     paddingBottom: 10,
+  },
+  cameraContainer: {
+    position: 'relative',
+    borderWidth: 1,
+    borderColor: '#023436',
+    zIndex: 1,
+  },
+  blueBorderTopLeft: {
+    position: 'absolute',
+    top: -3,
+    left: -3.5,
+    zIndex: 10,
+  },
+  blueBorderTopRight: {
+    position: 'absolute',
+    top: -3,
+    right: -3.5,
+    zIndex: 10,
+  },
+  blueBorderBottomLeft: {
+    position: 'absolute',
+    bottom: -3,
+    left: -3.5,
+    zIndex: 10,
+  },
+  blueBorderBottomRight: {
+    position: 'absolute',
+    bottom: -3,
+    right: -3.5,
+    zIndex: 10,
   },
   camera: {
     flex: 1,
